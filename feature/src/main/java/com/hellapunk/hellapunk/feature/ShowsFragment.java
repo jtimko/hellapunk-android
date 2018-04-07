fa173e172d7604f5e6bd048c1a9423456695b385
jtimko <justin@whisperfront.com>
2018-04-06 17:25:45 -0700

    building layout

--- feature/src/main/java/com/hellapunk/hellapunk/feature/ShowsFragment.java
+++ feature/src/main/java/com/hellapunk/hellapunk/feature/ShowsFragment.java
@@ -4,9 +4,16 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 
 public class ShowsFragment extends Fragment {
@@ -15,6 +22,23 @@ public class ShowsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_shows, container, false);
+        View v = inflater.inflate(R.layout.fragment_shows, container, false);
+
+        String url = "http://dev.jtimko.com/apps/hellapunk.php";
+        StringRequest sr = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.i("TAG", response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                //null
+            }
+        });
+        Volley.newRequestQueue(getContext()).add(sr);
+
+        return v;
+
     }
 }
